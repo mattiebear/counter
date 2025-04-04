9a4fe3d3b14a555e9a78ad737c3b0ce4f7aa02b5
Matthew Sells <matthew.sells@proton.me>
2025-04-04 11:55:54 -0500

    Add warning about counter implementation limitations

--- lib/counter.ex
+++ lib/counter.ex
@@ -1,6 +1,10 @@
 defmodule Counter do
   @moduledoc """
   The primary API for interacting with a counter process.
+
+  This is not an ideal implementation of a counter process. It uses a simple
+  process to keep track of the count, but it does not handle errors or
+  concurrency issues well. I'll make a more robust one in a new application.
   """
 
   def start(initial_count) do
