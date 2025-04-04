301d15b0a0197ebc5e0acd4e7282a5b9611295e4
Matthew Sells <matthew.sells@proton.me>
2025-04-04 11:30:50 -0500

    Add counter API integration test

--- test/counter_test.exs
+++ test/counter_test.exs
@@ -1,2 +1,14 @@
 defmodule CounterTest do
+  use ExUnit.Case
+  doctest Counter
+
+  test "advance counter through API" do
+    pid = Counter.start(0)
+    assert Counter.state(pid) == 0
+
+    Counter.tick(pid)
+    Counter.tick(pid)
+
+    assert Counter.state(pid) == 2
+  end
 end
